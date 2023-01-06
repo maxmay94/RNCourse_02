3520bd590d14604d6d23523f042cd1eaacc750ca
maxmay94 <maxmay94@gmail.com>
2023-01-06 17:08:40 -0500

    begin messing with style

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import PrimaryButton from "../components/PrimaryButton"
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -20,10 +20,22 @@ const styles = StyleSheet.create({
     marginHorizontal: 24,
     backgroundColor: '#72063c',
     borderRadius: 8,
-    elevation: 4,
+    elevation: 4, // elevation is all you need for basic andriod shadow
+    //ios needs all 4 
     shadowColor: 'black',
     shadowOffset: {width: 0, height: 2},
     shadowRadius: 6,
     shadowOpacity: .25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center'
   }
 })
