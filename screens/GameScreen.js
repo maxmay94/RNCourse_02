ac7248a174fccd9d66346953ec3f0612493cc226
maxmay94 <maxmay94@gmail.com>
2023-01-10 11:41:54 -0500

    change + - with icons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,6 @@
 import { View, StyleSheet, Alert } from "react-native"
 import { useState, useEffect } from "react"
+import { Ionicons } from '@expo/vector-icons'
 
 import NumberContainer from "../components/game/NumberContainer"
 import Title from "../components/ui/Title"
@@ -50,10 +51,14 @@ function GameScreen({ userNumber, onGameOver }) {
         <InstructionText style={styles.instructionText}>Higher or lower?</InstructionText>
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+              <Ionicons name="md-remove" size={24} color='white' />
+            </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+              <Ionicons name="md-add" size={24} color='white' />
+            </PrimaryButton>
           </View>
         </View>
       </Card>
