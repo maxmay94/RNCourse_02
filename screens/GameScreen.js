8c0e1d2f05b119a4672c155a5ef871f13d4e0834
maxmay94 <maxmay94@gmail.com>
2023-01-11 15:29:17 -0500

    make a componenet for previous guess list

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -7,6 +7,7 @@ import Title from "../components/ui/Title"
 import PrimaryButton from "../components/ui/PrimaryButton"
 import Card from "../components/ui/Card"
 import InstructionText from "../components/ui/InstructionText"
+import GuessLogItem from "../components/game/GuessLogItem"
 
 function generateRandomNumberBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min
@@ -50,6 +51,8 @@ function GameScreen({ userNumber, onGameOver }) {
     setGuessRounds(prevGuessRounds => [newRndNumber, ...prevGuessRounds])
   } 
 
+  const guessRoundsListLength = guessRounds.length
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
@@ -72,7 +75,7 @@ function GameScreen({ userNumber, onGameOver }) {
       <View> 
         <FlatList 
           data={guessRounds} 
-          renderItem={(itemData) => <Text>{itemData.item}</Text>} 
+          renderItem={(itemData) => <GuessLogItem roundNumber={guessRoundsListLength - itemData.index} guess={itemData.item} />} 
           keyExtractor={(item) => item}
         />
       </View>
